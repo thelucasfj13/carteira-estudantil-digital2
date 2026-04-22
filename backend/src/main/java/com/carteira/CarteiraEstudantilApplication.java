63f8c41597591868404e86891bf7e680d852a01e
LucasJesus <lucasjesusazapfy@gmail.com>
2026-04-22 17:29:06 -0300

    feat: backend/src/main/java/com/carteira/CarteiraEstudantilApplication.java - Fabrica Express

---
+++ backend/src/main/java/com/carteira/CarteiraEstudantilApplication.java
@@ -0,0 +1,11 @@
+package com.carteira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraEstudantilApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarteiraEstudantilApplication.class, args);
+    }
+}
